4fd51c8e4a6c51657eae5053c9c2048fcb797150
Emmanuel Pusso <emmanuel.pusso@gmail.com>
2024-01-31 13:59:50 -0300

    Improve the Link creation process

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -17,46 +17,16 @@ class LinksController < ApplicationController
   def new
     @link = Link.new
   end
-
+ 
   # GET /links/1/edit
   def edit
   end
 
-  # GET /links/1/report
-  # @visits_search include visits for Report #1 (if no filter was applied, return all visits, otherwise, it returns the subset of visits that match the filters)
-  # @visits_count include visits for Report #2 (returns a a hash where keys are dates and values are the counts of visits for each day for the specified link)
-  def report
-    
-    # @visits_count include visits for Report #2 
-    @visits_count = @visits.group(:visited_at).count
-
-    # @visits_search include visits for Report #1
-    # By default returns all visits (when NO filter is applied)
-    @visits_search = @visits
-    # Allows search by ip_address with a partial match
-    @visits_search  = @visits_search .where('ip_address LIKE ?', "%#{params[:ip_address]}%") if params[:ip_address].present?
-    # Allows search by date range, also searching bigger than a date or less than a date (at least one is required)
-    if params[:start_date].present? && params[:end_date].present? && params[:start_date] > params[:end_date]
-      flash[:error] = "End Date must be after Start Date"
-      redirect_back fallback_location: root_path
-    elsif params[:start_date].present? || params[:end_date].present?
-        from = params[:start_date].present? ? params[:start_date] : @link.visits.minimum(:visited_at).strftime("%Y-%m-%d")
-        to = params[:end_date].present? ? params[:end_date] : Date.today.strftime("%Y-%m-%d")
-        @visits_search  = @visits_search .where(visited_at: from..to)
-      end
-  end
-
-  def clear
-    @visits_search = @visits
-    render :report
-  end
-
   # POST /links or /links.json
   def create
-
     # Create the link with form parameters
       # dynamically convert a string into a class name and instantiate an object
-    @link = Object.const_get(link_params[:type]).new(link_params)
+    @link = Object.const_get(params[:link_type]).new(link_params)
     
     #The user_id must be obtained from the user who is currently logged in
     @link.assign_attributes(user_id: @current_user.id)  
@@ -129,6 +99,35 @@ class LinksController < ApplicationController
       end
     end
   
+  # GET /links/1/report
+  # @visits_search include visits for Report #1 (if no filter was applied, return all visits, otherwise, it returns the subset of visits that match the filters)
+  # @visits_count include visits for Report #2 (returns a a hash where keys are dates and values are the counts of visits for each day for the specified link)
+  def report
+    
+    # @visits_count include visits for Report #2 
+    @visits_count = @visits.group(:visited_at).count
+
+    # @visits_search include visits for Report #1
+    # By default returns all visits (when NO filter is applied)
+    @visits_search = @visits
+    # Allows search by ip_address with a partial match
+    @visits_search  = @visits_search .where('ip_address LIKE ?', "%#{params[:ip_address]}%") if params[:ip_address].present?
+    # Allows search by date range, also searching bigger than a date or less than a date (at least one is required)
+    if params[:start_date].present? && params[:end_date].present? && params[:start_date] > params[:end_date]
+      flash[:error] = "End Date must be after Start Date"
+      redirect_back fallback_location: root_path
+    elsif params[:start_date].present? || params[:end_date].present?
+        from = params[:start_date].present? ? params[:start_date] : @link.visits.minimum(:visited_at).strftime("%Y-%m-%d")
+        to = params[:end_date].present? ? params[:end_date] : Date.today.strftime("%Y-%m-%d")
+        @visits_search  = @visits_search .where(visited_at: from..to)
+    end
+  end
+
+  def clear
+    @visits_search = @visits
+    render :report
+  end
+
    
   private
 
